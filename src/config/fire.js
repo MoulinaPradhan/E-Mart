72a329b9a63111164742e8fd14295ce43e3b7df4
moulina pradhan <pmoulina76@gmail.com>
2021-03-27 00:10:42 +0530

    firebase

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase";
 import "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+  apiKey: "AIzaSyAT8gVCYiuNWhYfsiZWaIPlcoFzU7ur_6A",
+  authDomain: "shopping-site-2f091.firebaseapp.com",
+  databaseURL: "https://shopping-site-2f091.firebaseio.com",
+  projectId: "shopping-site-2f091",
+  storageBucket: "shopping-site-2f091.appspot.com",
+  messagingSenderId: "316192273235",
+  appId: "1:316192273235:web:f812d48803b13029b4e7a6"
   };
  
   const fire=firebase.initializeApp(firebaseConfig);
